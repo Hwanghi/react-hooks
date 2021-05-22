010955614617923679234ec6a9d65fa9334deb9b
Hwanghi <heeda512@naver.com>
2021-05-22 10:34:43 +0900

    nomad Hooks!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+const useConfirm = (message = "", callback) => {
+  if (typeof callback !== "function") {
+    return;
+  }
+  const confirmAction = () => {
+    if (window.confirm(message)) {
+      callback();
+    }
+  };
+  return confirmAction;
+};
+const App = () => {
+  const deleteWorld = () => {
+    console.log("Deleting the world...");
+  };
+  const confirmDelete = useConfirm(
+    "Are you sure",
+    deleteWorld
+  );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={confirmDelete}>
+        Delete the world
+      </button>
     </div>
   );
-}
+};
 
 export default App;
